119c31e30b4a67ad0f88b33630179c631a397484
ryibeiro <125921828+ryibeiro@users.noreply.github.com>
2023-02-20 17:40:51 -0300

    Códigos Simples em Python - Basics Codes in Python

---
+++ exercicios_python1.py
@@ -0,0 +1,20 @@
+# irei definir uma variavel nome
+nome = 'Ribeiro'
+# irei definir para exibir essa variavel nome
+print (nome),
+# irei definir uma variavel sobre a idade de Ribeiro
+idade = 20
+# irei definir para exibir essa variavel sobre a idade de Ribeiro
+print (idade),
+# irei definir uma variavel sobre a data de nascimento de Ribeiro
+ano_de_nascimento = 2022 - idade
+# irei definir para exibir a data de nascimento de Ribeiro
+print (ano_de_nascimento),
+# irei definir se Ribeiro é maior de idade ou não!
+maior_de_idade = idade >= 18
+# irei exibir se Ribeiro é ou não maior de idade
+print (maior_de_idade),
+#irei definir a altura de Ribeiro
+altura = 1.78
+# irei exibir a altura de Ribeiro
+print (altura),
