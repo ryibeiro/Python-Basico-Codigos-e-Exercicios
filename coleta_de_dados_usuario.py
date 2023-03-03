94b0a64d2cdc47a68c144cd8bbec5cb3b2fb7f96
Matheus Ribeiro <125921828+ryibeiro@users.noreply.github.com>
2023-03-03 13:46:47 -0300

    coleta de dados simples do usuário - Exercicio
    Códigos simples e Exercicio básico de coleta de dados do usuário, com uma leve interação.

---
+++ coleta_de_dados_usuario.py
@@ -0,0 +1,13 @@
+# exercicio de interação e coleta de informações do usuario
+
+nome = input ('qual o seu nome ser humano?: ')
+print (f'o seu nome é legal! {nome}')
+
+numero1 = input ('digite um numero que goste ai!: ')
+numero2 = input ('digite outro numero que goste ai!: ')
+
+
+intnumero1 = int(numero1)
+intnumero2 = int(numero2)
+
+print (f'a soma dos números legais é: {intnumero1 + intnumero2}')
